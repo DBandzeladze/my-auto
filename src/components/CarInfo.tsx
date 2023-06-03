d8a66fb1cb3ffb33003028fdf513eb25bde310ed
Davit <bandzeladze.daviti.dato@gmail.com>
2023-06-03 21:47:46 +0400

    with photos

--- src/components/CarInfo.tsx
+++ src/components/CarInfo.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { CarInfoDataType } from "../types/types";
 const CarInfo = (props: CarInfoDataType)=>{
+    const str = `https://static.my.ge/myauto/photos/${props.photo}/thumbs/${props.car_id}_1.jpg?v=${props.photo_ver}`
     return (
         <div className="container">
-            <div className="Photo">
-            </div>
+            <img src={str} className="img-thumbnail" alt="no Photo" width="200" height="150">
+            </img>
             <div className="info">
-                <div className="row1">{props.man_id},{props.model_id},{props.prod_year},</div>
-                <div className="row2">{props.fuel_type_id},{props.car_run},{props.price}</div>
-                <div className="row3">{props.drive_type_id},{props.right_wheel}</div>
+                <div className="row">
+                    <div className="col-sm-4">{props.man_id}</div>
+                    <div className="col-sm-4">{props.model_id}</div>
+                    <div className="col-sm-4">{props.prod_year}წ</div>
+                </div>
+                <div className="row">{props.fuel_type_id},{props.car_run},{props.price}</div>
+                <div className="row">{props.drive_type_id},{props.right_wheel}</div>
             </div>
         </div>
     )
