f0f000c73f9a6b485d743ff14562bede0a9f3239
Davit <bandzeladze.daviti.dato@gmail.com>
2023-06-04 16:01:27 +0400

    car info almost finished

--- src/components/CarInfo.tsx
+++ src/components/CarInfo.tsx
@@ -30,22 +30,90 @@ const CarInfo = (props: CarInfoDataType)=>{
     }, []);
 
     const wheelPosition = props.right_wheel ? "მარჯვენა" : "მარცხენა";
-    const str = `https://static.my.ge/myauto/photos/${props.photo}/thumbs/${props.car_id}_1.jpg?v=${props.photo_ver}`
+  const getFuelType = (fuelTypeId: number) => {
+    switch (fuelTypeId) {
+      case 2:
+        return "ბენზინი";
+      case 3:
+        return "დიზელი";
+      case 6:
+        return "ჰიბრიდი";
+      default:
+        return "ელექტრო";
+    }
+  };
+
+  // Mapping drive types
+  const getDriveType = (gearType: number) => {
+    switch (gearType) {
+      case 2:
+        return "ავტომატიკა";
+      case 1:
+        return "მექანიკა";
+      case 3:
+        return "ტიპტრონიკი";
+      case 4:
+        return "ვარიატორი";
+      default:
+        return "";
+    }
+  };
+  const Ganbajeba = props.customs_passed ? "განბაჟებული" : "განბაჟება ";
+
+    const str = `https://static.my.ge/myauto/photos/${props.photo}/thumbs/${props.car_id}_1.jpg?v=${props.photo_ver}`;
     if (loading) {
         return <></>;
     }
+     // Calculate time difference
+     const currentTime = new Date();
+     const orderDate = new Date(props.order_date);
+     const timeDifferenceMinutes = Math.floor((currentTime.getTime() - orderDate.getTime()) / (1000 * 60));
+     const timeDifferenceHours = Math.floor(timeDifferenceMinutes / 60);
+     const timeDifferenceDays = Math.floor(timeDifferenceHours / 24);
+     const timeDifferenceMonths = Math.floor(timeDifferenceDays / 30);
+     const timeDifferenceYears = Math.floor(timeDifferenceMonths / 12);
+ 
+     let timeDifferenceText = "";
+     if (timeDifferenceYears > 0) {
+       timeDifferenceText = `${timeDifferenceYears} წლის წინ`;
+     } else if (timeDifferenceMonths > 0) {
+       timeDifferenceText = `${timeDifferenceMonths} თვის წინ`;
+     } else if (timeDifferenceDays > 0) {
+       timeDifferenceText = `${timeDifferenceDays} დღის წინ`;
+     } else if (timeDifferenceHours > 0) {
+       timeDifferenceText = `${timeDifferenceHours} საათის წინ`;
+     } else if (timeDifferenceMinutes === 0) {
+       timeDifferenceText = `წუთის წინ`;
+     } else {
+        timeDifferenceText = `${timeDifferenceMinutes} წუთის წინ`;
+     }
+     let carPrice : string = "";
+     if (props.price === 0) {
+        carPrice = "ფასი შეთანხმებით"
+     }
+     else {
+        carPrice = `${props.price}`
+     }
+     let ForRent : string = "";
+     if (props.for_rent) {
+        ForRent = "ქირავდება"
+     }
+     else {
+        ForRent = ""
+     }
+ 
     return (
         <div className="container">
-            <img src={str} className="img-thumbnail" alt="no Photo" width="200" height="150">
-            </img>
+            <img src={str} className="img-thumbnail" alt="no Photo" width="200" height="150" />
             <div className="info">
                 <div className="row">
-                    <div className="col-sm-6">{`${manufacturer.filter((info) => info.man_id === props.man_id.toString()).find((info) => info.man_id === props.man_id.toString())?.man_name} ${modelList.filter((info)=>info.model_id === props.model_id).find((info)=>info.model_id===props.model_id)?.model_name}`}</div>
-                    {/* <div className="col-sm-4"></div> */}
-                    <div className="col-sm-6">{props.prod_year}წ</div>
+                    <div className="col-sm-4">{`${ForRent} ${manufacturer.filter((info) => info.man_id === props.man_id.toString()).find((info) => info.man_id === props.man_id.toString())?.man_name} ${modelList.filter((info)=>info.model_id === props.model_id).find((info)=>info.model_id===props.model_id)?.model_name} ${props.car_model}`}</div>
+                    <div className="col-sm-4">{props.prod_year}წ</div>
+                    <div className="col-sm-4">{Ganbajeba}</div>
                 </div>
-                <div className="row">{props.fuel_type_id},{props.car_run},{props.price}</div>
-                <div className="row">{props.drive_type_id},{wheelPosition}</div>
+                <div className="row">{`${(props.engine_volume/1000).toPrecision(2)} ${getFuelType(props.fuel_type_id)}`},{props.car_run} კმ,{carPrice}</div>
+                <div className="row">{getDriveType(props.gear_type_id)}, {wheelPosition}</div>
+                <div className="row">{props.views} ნახვა   .   {timeDifferenceText}</div>
             </div>
         </div>
     )
